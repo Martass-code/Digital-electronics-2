24618569d252bc72cc418d1b7e4a2a92516391a8
Martin <you@example.com>
2020-10-05 20:59:41 +0200

    added coments to programs, added HW word-file

--- 02_leds/02-leds-HM/02-leds-HM/main.c
+++ 02_leds/02-leds-HM/02-leds-HM/main.c
@@ -1,5 +1,6 @@
 /* Defines -----------------------------------------------------------*/
-#define BLINK_DELAY 250
+
+#define BLINK_DELAY 250 //speed of blinking
 #ifndef F_CPU
 #define F_CPU 16000000      // CPU frequency in Hz required for delay
 #endif
@@ -10,39 +11,29 @@
 
 int main(void)
 {
-
-	DDRC |= (0b00111111); 
-	PORTC |= (0b00000000);
-	//PORTC &= ~(1<<LED_RED);
+	//LEDs are on pins PC5 to PC0 (6 total) alternative DDRC |= (1<<PC5)|(1<<PC4)|(1<<PC3)...
+	DDRC |= (0b00111111); //set all LEDs as output
+	PORTC &= (0b11000000); //set off all LEDs  
 	
-	
-
-
- uint8_t u = 0b00010000;
+	uint8_t u = 0b00010000; 
     // Infinite loop
     while (1)
     {
-
-        // Pause several milliseconds
+		//rewriting PC7 (not used) and PC6 (defined as input with ext. pull up) does not matter
+		
+        //swift lighting LED to left
 		for(uint8_t i = 0; i<6; i++)
 		{
 		PORTC = (1<<i);
 		_delay_ms(BLINK_DELAY);	
 		}
-		
+		//swift lighting LED to right
 		for(int8_t i = 0; i<4; i++)
 		{
 			PORTC = (u>>i);
 			_delay_ms(BLINK_DELAY);
-		}	
-	
-		
-		
-		
-		
-        
+		}		  
     }
-
     // Will never reach this
     return 0;
 }
--- 02_leds/02-leds/02-leds/main.c
+++ 02_leds/02-leds/02-leds/main.c
@@ -1,7 +1,7 @@
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN   PB5     // AVR pin where green LED is connected
-#define LED_RED PC0
-#define PUSH_BUTTON PD0
+#define LED_RED PC0 // AVR pin where red LED is connected
+#define PUSH_BUTTON PD0 // AVR pin where pushbutton is connected
 #define BLINK_DELAY 500
 #ifndef F_CPU
 #define F_CPU 16000000      // CPU frequency in Hz required for delay
@@ -18,35 +18,26 @@
  */
 int main(void)
 {
-    /* GREEN LED */
-    // Set pin as output in Data Direction Register...
-    DDRB |= (1<<LED_GREEN);
-	PORTB &= ~(1<<LED_GREEN);
-    // ...and turn LED off in Data Register
-	DDRC |= (1<<LED_RED);
-	PORTC &= ~(1<<LED_RED);
+    /* GREEN LED */    
+    DDRB |= (1<<LED_GREEN); // Set pin as output in Data Direction Register...
+	PORTB &= ~(1<<LED_GREEN); // ...and turn LED off in Data Register
+	/* RED LED */
+	DDRC |= (1<<LED_RED); // Set pin as output in Data Direction Register...
+	PORTC &= ~(1<<LED_RED); // ...and turn LED off in Data Register
+	/* PUSHBUTTON */
+	DDRD &= ~(1<<PUSH_BUTTON); // Set pin as input in Data Direction Register...
+	PORTD |= (1<<PUSH_BUTTON); // Set pull-up resistor
 	
-	DDRD &= ~(1<<PUSH_BUTTON);
-	PORTD |= (1<<PUSH_BUTTON);
-	
-
-    /* second LED */
-    // WRITE YOUR CODE HERE
-
-    /* push button */
-    // WRITE YOUR CODE HERE
-
-    // Infinite loop
+	    // Infinite loop
     while (1)
     {
-		if(bit_is_clear(PIND, PUSH_BUTTON))
+		if(bit_is_clear(PIND, PUSH_BUTTON)) //if button is pressed blink with LEDs
 		{
         // Pause several milliseconds
         _delay_ms(BLINK_DELAY);
 		PORTB ^= (1<<LED_GREEN);
 		PORTC ^= (1<<LED_RED);
 		}
-
         
     }
 
